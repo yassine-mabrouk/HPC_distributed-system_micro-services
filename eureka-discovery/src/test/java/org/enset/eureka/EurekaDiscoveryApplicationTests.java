9f8323661d493809b8c03192baf0579b59730457
yassine-mabrouk <mabroukyassine70@gmail.com>
2021-11-12 10:29:30 +0100

    update readme

---
+++ eureka-discovery/src/test/java/org/enset/eureka/EurekaDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enset.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
